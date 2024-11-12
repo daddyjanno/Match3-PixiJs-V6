04a822e34a588d33413277b028fd957271d8b465
daddyjanno <jn.drugmand@gmail.com>
2024-11-12 12:05:39 +0100

    Game

--- src/scripts/game/Game.js
+++ src/scripts/game/Game.js
@@ -11,11 +11,58 @@ export class Game extends Scene {
 
         this.board = new Board()
         this.container.addChild(this.board.container)
+        this.board.container.on('tile-touch-start', this.onTileClick.bind(this))
     }
     createBackground() {
         this.bg = App.sprite('bg')
         this.bg.width = window.innerWidth
         this.bg.height = window.innerHeight
         this.container.addChild(this.bg)
+    }
+
+    onTileClick(tile) {
+        if (this.disabled) {
+            return
+        }
+        if (this.selectedTile) {
+            if (!this.selectedTile.isNeighbour(tile)) {
+                this.clearSelection(tile)
+                this.selectTile(tile)
+            } else {
+                this.swap(this.selectedTile, tile)
+            }
+        } else {
+            this.selectTile(tile)
+        }
+    }
+    selectTile(tile) {
+        this.selectedTile = tile
+        this.selectedTile.field.select()
+    }
+
+    swap(selectedTile, tile) {
+        // lock the board to prevent tiles from moving again while the animation is already running
+        this.disabled = true
+        // hide the "field-selected" frame from the field of the selectedTile object
+        this.clearSelection()
+        // place the selectedTile sprite one layer higher than the tile sprite
+        selectedTile.sprite.zIndex = 2
+
+        // move selectedTile to tile position
+        selectedTile.moveTo(tile.field.position, 0.2)
+        // move tile to selectedTile position
+        tile.moveTo(selectedTile.field.position, 0.2).then(() => {
+            // after motion animations complete:
+            // change the values of the field properties in the tile objects
+            // change the values of the tile properties in the field objects
+            this.board.swap(selectedTile, tile)
+            this.disabled = false
+        })
+    }
+    clearSelection() {
+        if (this.selectedTile) {
+            this.selectedTile.field.unselect()
+            this.selectedTile = null
+        }
     }
 }
