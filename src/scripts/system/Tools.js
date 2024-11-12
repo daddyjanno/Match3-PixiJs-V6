3ac915e9a7930f52c9f4e24726c3ca15429c6855
daddyjanno <jn.drugmand@gmail.com>
2024-11-12 11:15:47 +0100

    randomNumber in Tools

--- src/scripts/system/Tools.js
+++ src/scripts/system/Tools.js
@@ -11,4 +11,12 @@ export class Tools {
 
         return files
     }
+    static randomNumber(min, max) {
+        if (!max) {
+            max = min
+            min = 0
+        }
+
+        return Math.floor(Math.random() * (max - min + 1) + min)
+    }
 }
